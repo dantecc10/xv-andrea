90f156b43d482f6d970e6ffd9e028a1eebc0bcd1
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2025-12-22 01:07:47 -0600

    added sql connections test with env secrets

---
+++ attendance/php-scripts/connection.php
@@ -0,0 +1,18 @@
+<php
+    // Importar variables de entorno desde el archivo .env
+    require_once (__DIR__ . '/../vendor/autoload.php');
+    $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
+    $dotenv->load();
+
+    $servername = $_ENV['DB_SERVER'];
+    $username = $_ENV['DB_USERNAME'];
+    $password = $_ENV['DB_PASSWORD'];
+    $dbname = $_ENV['DB_NAME'];
+
+    // Crear conexión
+    $connection = new mysqli($servername, $username, $password, $dbname);
+    // Verificar conexión
+    if ($connection->connect_error) {
+        die("Conexión fallida: " . $connection->connect_error);
+    }
+?>
