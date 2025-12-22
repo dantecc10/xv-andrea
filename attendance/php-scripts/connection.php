3bc2eee0b4df97b13b5b03ecba772290ad143e16
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2025-12-22 01:12:57 -0600

    ping

--- attendance/php-scripts/connection.php
+++ attendance/php-scripts/connection.php
@@ -1,18 +1,25 @@
 <?php
-    // Importar variables de entorno desde el archivo .env
-    require_once (__DIR__ . '/../../vendor/autoload.php');
-    $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
-    $dotenv->load();
+// Importar variables de entorno desde el archivo .env
+require_once(__DIR__ . '/../../vendor/autoload.php');
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
+$dotenv->load();
 
-    $servername = $_ENV['DB_SERVER'];
-    $username = $_ENV['DB_USERNAME'];
-    $password = $_ENV['DB_PASSWORD'];
-    $dbname = $_ENV['DB_NAME'];
+if($_ENV['ENVIRONMENT'] === 'DEVELOPMENT'){
+    error_reporting(E_ALL);
+    ini_set('display_errors', 1);
+} else {
+    error_reporting(0);
+    ini_set('display_errors', 0);
+}
 
-    // Crear conexión
-    $connection = new mysqli($servername, $username, $password, $dbname);
-    // Verificar conexión
-    if ($connection->connect_error) {
-        die("Conexión fallida: " . $connection->connect_error);
-    }
-?>
+$servername = $_ENV['DB_SERVER'];
+$username = $_ENV['DB_USERNAME'];
+$password = $_ENV['DB_PASSWORD'];
+$dbname = $_ENV['DB_NAME'];
+
+// Crear conexión
+$connection = new mysqli($servername, $username, $password, $dbname);
+// Verificar conexión
+if ($connection->connect_error) {
+    die("Conexión fallida: " . $connection->connect_error);
+}
