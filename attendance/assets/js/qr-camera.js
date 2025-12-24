48a68729bd0a95835c35692b2b1c172d2c6c53df
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2025-12-23 19:33:41 -0600

    fixing js

--- attendance/assets/js/qr-camera.js
+++ attendance/assets/js/qr-camera.js
@@ -1,25 +1,22 @@
-//Inicializo la cámara y el lector de QR
-const result = document.getElementById("result");
-const qrCodeScanner = new Html5Qrcode("reader");
-const linkElement = document.getElementById("qr-link");
+let qrCodeScanner;
+let initialized = false;
 
 function startScanner() {
-    qrCodeScanner.start({
-        facingMode: "environment"
-    }, // cámara trasera en móviles
-        {
-            fps: 10,
-            qrbox: 250
-        },
-        (decodedText) => {
-            result.innerHTML = "<a class='bg-success text-white btn' href='" + decodedText + "'>VER PASE</a>";
-            linkElement.textContent = decodedText;
 
-            // Detiener el escáner después de una lectura exitosa
+    if (!initialized) {
+        qrCodeScanner = new Html5Qrcode("reader");
+        initialized = true;
+    }
+
+    qrCodeScanner.start(
+        { facingMode: "environment" },
+        { fps: 10, qrbox: 250 },
+        (decodedText) => {
+
+            document.getElementById("qr-link").innerHTML =
+                `<a class="bg-success text-white btn" href="${decodedText}">VER PASE</a>`;
+
             qrCodeScanner.stop();
-        },
-        (errorMessage) => {
-            // errores silenciosos de lectura
         }
     );
 }
