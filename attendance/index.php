0127f9fcefcbfe40aa99bda502ed410c35e4f573
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2025-12-25 22:50:17 -0600

    set right title

--- attendance/index.php
+++ attendance/index.php
@@ -6,7 +6,7 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
-    <title>Home - XV Andrea</title>
+    <title>Inicio - XV Andrea</title>
     <meta name="description" content="Asistencia a los XV años de Andrea">
     <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat:400,700&amp;display=swap">
